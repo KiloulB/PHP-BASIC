af5c4fdde2671d5ba2727512aa026c53c398cc9d
KiloulB <kiloulb@gmail.com>
2020-11-10 10:31:50 +0100

    Alles af

--- opdracht/producten.php
+++ opdracht/producten.php
@@ -81,7 +81,7 @@ $prijs = array("0,25", "1,25", "1,30", "0,44", "0,35", "1,00", "0,50", "0,75", "
     <p><label>leeftijd: </label><input type="text" name="leeftijd"></p><!-- dit laten staan! -->
     <p><button type="submit" name="submit">Bereken totaal</button></p><!-- dit laten staan! -->
     <p>voor <?php getAmount(); ?> x het product: <?php getProductNr(); ?> betaal je: </p>
-    <p>je bent <?php getAge(); ?> je korting is: </p>
+    <p>je bent <?php getAge(); ?> je korting is: <?php getKorting();?></p>
 
     </form><!-- dit laten staan! -->
 </div>
